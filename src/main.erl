25de9871a544584a341d50ec01b3f6d48703c9df
sawar7577 <sawar@DESKTOP-6D81TUR.localdomain>
2020-11-12 14:21:41 +0530

    helpers

--- src/main.erl
+++ src/main.erl
@@ -1,5 +1,5 @@
 -module(main).
--export([start/0, hello/1, proc_run/5]).
+-export([start/0, hello/1, proc_run/4]).
 
 -define(Inf, 100000000).
 -define(GetElement(R,C,N,L), list:nth(R*N + C, L)).
@@ -31,26 +31,62 @@ make_displs(NumVertices, NumProcs) ->
     lists:append(InitDispls, [lists:last(InitDispls)+PerProc+RemProc]).
 
 
-spawner(NumVertices, NumProcs, StartRow, EndRow) ->
-    spawn(main, proc_run, [NumVertices, NumProcs, StartRow, EndRow, []]).
+spawner(NumVertices, NumProcs, Rank) ->
+    spawn(main, proc_run, [NumVertices, NumProcs, Rank, []]).
 
-proc_run(NumVertices, NumProcs, StartRow, EndRow, LocalData) ->
+get_proc_rank(Displs, RowNumber)->
+    get_proc_rank(Displs, 1, RowNumber).
+
+get_proc_rank(Displs, Rank, RowNumber) ->
+    case lists:nth(Rank, Displs) =< RowNumber of
+        true ->
+            get_proc_rank(Displs, Rank+1, RowNumber);
+        false ->
+            Rank-1
+    end.
+
+get_minimum_vert(Dist) ->
+    get_minimum_vert(Dist, 1, {?Inf, -1}).
+
+get_minimum_vert([], Index, _) ->
+    {?Inf, Index};
+
+get_minimum_vert([H | T], Index, MinTup) ->
+    RetList = get_minimum_vert(T, Index+1, MinTup),
+    case element(1, RetList) < H of 
+        true ->
+            RetList;
+        false ->
+            {H, Index}
+        end.
+
+
+proc_run(NumVertices, NumProcs, Rank, LocalData) ->
     % hello(LocalData),
     % hello("self id\n"),
     % hello([self(), LocalData]),
     receive
         {"input", Data} ->
-            proc_run(NumVertices, NumProcs, StartRow, EndRow, lists:append(LocalData, Data));
+            proc_run(NumVertices, NumProcs, Rank, lists:append(LocalData, Data));
         {"other", Data} ->
-            proc_run(NumVertices, NumProcs, StartRow, EndRow, LocalData);
-        {"source", Source} ->
-            dijkstra(NumVertices, NumProcs, StartRow, EndRow, LocalData, Source);
+            proc_run(NumVertices, NumProcs, Rank, LocalData);
+        % {"source", Source} ->
+        %     dijkstra(NumVertices, NumProcs, StartRow, EndRow, LocalData, Source);
         stop ->
             % hello("stop"),
             hello([self(), LocalData]),
             ok
     end.
-    
+
+send_to_neighbours(Neighs, Msg) ->
+    lists:foreach(
+            fun(X) ->
+                [{_, SPid}] = ets:lookup(procTable, X),
+                SPid ! Msg
+            end,
+            Neighs
+        ).
+
 read_and_send(Device, Id) ->
     % hello("Id"),
     % hello(Id),
@@ -77,39 +113,58 @@ distribute_graph(Device, NumVertices, NumProcs, Displs, CurRow, CurIndex) ->
             read_and_send(Device, CurIndex),
             distribute_graph(Device, NumVertices, NumProcs, Displs, CurRow + 1, CurIndex);
         true ->
-            ets:insert(
-                procTable, 
-                {
-                    CurIndex + 1, 
-                    spawner(NumVertices, NumProcs, lists:nth(CurIndex, Displs)+1, lists:nth(CurIndex+1, Displs))
-                }
-            ),
+            register_proc(
+                    spawner(NumVertices, NumProcs, CurIndex+1),                
+                    CurIndex+1
+                ),
             distribute_graph(Device, NumVertices, NumProcs, Displs, CurRow, CurIndex+1)
     end.
     
 
-updateDist(ResultVec, LocalData, Source, StartRow, EndRow) ->
+% updateDist(ResultVec, LocalData, Source, StartRow, EndRow) ->
     
+% collect(ReduceProc) ->
+%     ReduceProc ! {collect, self()}
 
-dijkstra(NumVertices, NumProcs, StartRow, EndRow, LocalData, Source) ->
+map_task(NumVertices, NumProcs, Rank, LocalData, LocalDist) ->
+    get_minimum_vert(LocalDist).
 
-    LocalVec = list:duplicate(?Inf, EndRow - StartRow),
-    ResultVec = list:duplicate(?Inf, NumVertices),
-    IsSource = fun (StartRow, EndRow, Source) ->
-            if Source < StartRow ->
-                false;
-            Source > EndRow ->
-                false;
-            true ->
-                true
-            end
-        end,
-    Source = IsSource(StartRow, EndRow, Source),
-    if Source ->
-        NewRes = updateDist(ResultVec, LocalData, Source, StartRow, EndRow)
+reduce_task(NumVertices, NumProcs, Rank, LocalData, LocalDist)->
+    get_minimum_vert(LocalDist).
+
+
+% init_dijkstra(NumVertices, NumProcs, StartRow, EndRow, Source) ->
+%     case { StartRow =< Source , Source >= EndRow } of
+%         {true, true} ->
+
+%         {_, _} ->
+
+
+
+
+% dijkstra(NumVertices, NumProcs, StartRow, EndRow, LocalData, Source) ->
+
+%     LocalVec = list:duplicate(?Inf, EndRow - StartRow),
+%     ResultVec = list:duplicate(?Inf, NumVertices),
+%     IsSource = fun (StartRow, EndRow, Source) ->
+%             if Source < StartRow ->
+%                 false;
+%             Source > EndRow ->
+%                 false;
+%             true ->
+%                 true
+%             end
+%         end.
+%     % Source = IsSource(StartRow, EndRow, Source),
+%     % if Source ->
+%         % NewRes = updateDist(ResultVec, LocalData, Source, StartRow, EndRow)
     
-    end.
+%     % end.
 
+
+register_proc(Pid, Id) ->
+    ets:insert(procTable, {Id, Pid}),
+    ets:insert(idTable, {Pid, Id}).
 
                 
 start() ->
@@ -121,6 +176,9 @@ start() ->
     Displs = lists:append(make_displs(NumVertices, NumProcs), [ ?Inf ] ),
     
     ets:new(procTable, [named_table, public]),
+    ets:new(idTable, [named_table, public]),
+
+    
     GetData = fun F(Data, CurRow, EndRow) ->
                 if 
                     CurRow > EndRow ->
@@ -144,16 +202,21 @@ start() ->
 %[2, 4, 6, inf]
 % (4,3)
     hello([self(), LocalData]),
+    register_proc(self(), 1),
     distribute_graph(Device, NumVertices, NumProcs, Displs, lists:nth(1, Displs)+1, 1),
     % hello(NumVertices),
     % hello(NumProcs),
     % hello(Displs),  
-    [{_, SPid1}] = ets:lookup(procTable, 2),
-    SPid1 ! stop,
-    [{_, SPid2}] = ets:lookup(procTable, 3),
-    SPid2 ! stop,
-
+    % [{_, SPid1}] = ets:lookup(procTable, 2),
+    % SPid1 ! stop,
+    % [{_, SPid2}] = ets:lookup(procTable, 3),
+    % SPid2 ! stop,
+    send_to_neighbours([2, 3], stop),
+    hello(["hey", get_proc_rank(Displs, 2)]),
+    hello(["minvert", get_minimum_vert([5, 6, 7, 8, 9, 1, 2, 3])]),
     ets:delete(procTable),
+    ets:delete(idTable),
+
     file:close(Device).
 
     
